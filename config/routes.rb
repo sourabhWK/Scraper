5e6feaef2868dd01a9d4478bcf226ad53d73f008
sourabhWK <sourabh@webkorps.com>
2023-02-20 19:43:14 +0530

    removed minor bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :hockey_teams do
     match '/scrape', to: 'hockey_teams#scrape', via: :post, on: :collection
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
   root "hockey_teams#index"
 end
